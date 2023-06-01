4a11e12c041fe71d244820bee67d8764fdc7971d
Biganao <mfponte2006@gmail.com>
2023-06-01 18:14:53 -0300

    :sparkles: feat: added new functions and update UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,94 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/sidebar/Sidebar";
+import Layout from "./components/layout/Layout";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Shop from "./pages/shop/Shop";
+import Client from "./pages/client/Client";
+import Terms from "./pages/info/Terms";
+import Privacy from "./pages/info/Privacy";
+import Faq from "./pages/info/FAQ";
+import AddBalance from "./pages/addBalance/AddBalance";
+
 function App() {
-  return(
-    <div>
-      <h1 className="text-emerald-400 text-9xl">10k</h1>
-    </div>
-  )
+  return (
+    <Router>
+      <ToastContainer />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Sidebar>
+              <Layout>
+                <Dashboard />
+              </Layout>
+            </Sidebar>
+          }
+        />
+        <Route
+          path="/shops"
+          element={
+            <Sidebar>
+              <Layout>
+                <Shop />
+              </Layout>
+            </Sidebar>
+          }
+        />
+        <Route
+          path="/clients"
+          element={
+            <Sidebar>
+              <Layout>
+                <Client />
+              </Layout>
+            </Sidebar>
+          }
+        />
+        <Route
+          path="/add-balance"
+          element={
+            <Sidebar>
+              <Layout>
+                <AddBalance />
+              </Layout>
+            </Sidebar>
+          }
+        />
+        <Route
+          path="/terms"
+          element={
+            <Sidebar>
+              <Layout>
+                <Terms />
+              </Layout>
+            </Sidebar>
+          }
+        />
+        <Route
+          path="/privacy"
+          element={
+            <Sidebar>
+              <Layout>
+                <Privacy />
+              </Layout>
+            </Sidebar>
+          }
+        />
+        <Route
+          path="/faq"
+          element={
+            <Sidebar>
+              <Layout>
+                <Faq />
+              </Layout>
+            </Sidebar>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'persian-blue': '#2937bd',
+        'grape': '#5c39ba',
+        'rose-quartz': '#a6a3ae',
+        'eerie-black': '#1a1b1d',
+      },
+    },
   },
   plugins: [],
 };
