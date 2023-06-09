f4ae059a7a3fc6cc14100cee236c60b5e59e09bc
Biganao <mfponte2006@gmail.com>
2023-06-08 23:37:03 -0300

    :test_tube: test: server cors origin

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://festpay.vercel.app"],
     credentials: true,
   })
 );
