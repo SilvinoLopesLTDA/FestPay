d8d77e831892e21b24ceb4ec2d438cdda9ce97ca
Joisson Rocha <strattegiadev@gmail.com>
2023-06-22 22:04:32 -0300

    :bug: Bug fix in backend

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://festpay.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://festpay.vercel.app",
+      "https://festpay-kw22.onrender.com",
+    ],
     credentials: true,
+    allowedHeaders: ["Content-Type", "Cookie"],
   })
 );
 app.options("*", cors());
